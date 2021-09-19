efc1b3217fb20f859c14d7b1bd1e1acbf7228fc2
marfanigar <72514069+marfanigar@users.noreply.github.com>
2021-09-19 15:18:26 +0530

    Add files via upload

---
+++ TASK 2/src/Component/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <div>
+            <nav className="navbar navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">
+                        <img src="https://i.pinimg.com/originals/d5/73/4f/d5734fca2b464f0148c0043448e302a3.gif" alt="Logo" width="220" className="d-inline-block align-item-top"/>
+                        <p>We Make Air And Space Available For Everyone</p>
+                    </a>
+
+                </div>
+            </nav>
+
+        </div>
+    );
+}
+export default Navbar;
